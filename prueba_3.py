7a26e5d24d9db57996d64cdc471494d63e1c675d
kraorlm21 <am.leal@duocuc.cl>
2024-06-24 13:30:03 -0400

    Add files via upload

---
+++ prueba_3.py
@@ -0,0 +1,35 @@
+prueba_3 
+
+*********MUNDO LIBRO*******
+agregar_libros = []
+editar_libros = []
+reportes_de_libros = []
+while True:
+    opc = int(input("***ingrese una de las siguientes opciones
+    1) - mantenedor de libros
+    2) - reportes 
+    3) - salir        ""))
+
+
+
+
+
+    if opc== 1:
+        print("menu")
+    agregando_libros = int(input("ingrese el libro que deseas agregar: "))
+    agregar_libros.append(agregando_libros)
+    print(f"libros agregados") {agregar_libros}
+
+    if opc== 2:
+        editar_libro = int(input("ingrese el "))
+
+
+    if opc== 3:
+        libro_descartado = int(input("ingrese el ID del libro para eliminar: "))
+        agregar_libros.pop(libro_descartado)
+        print(f"el libro fue eliminado {agregar_libros}")
+    
+
+    if opc== 4:
+        if el ID de su libro es 
+    
